11b56854404c39433947b6a22a01f63b6f0d81f3
Alexis Valles <vallesvictor276@gmail.com>
2024-11-26 00:51:28 -0600

    second
    avanzando gente

--- SignUpScreen.js
+++ SignUpScreen.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
-import { View, Text, TextInput, Button, Alert, StyleSheet } from 'react-native';
+import { View, Text, TextInput, Alert, StyleSheet, ImageBackground, TouchableOpacity } from 'react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+
 
 export default function SignUpScreen({ navigation }) {
   const [username, setUsername] = useState('');
@@ -27,63 +28,135 @@ export default function SignUpScreen({ navigation }) {
   };
 
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Create Account</Text>
+    <ImageBackground
+      source={require('./assets/Back2.png')} 
+      style={styles.background}
+      resizeMode="cover"
+    >
+      <View>
+        <Text style={styles.title}>Create Account</Text>
 
-      <TextInput
-        style={styles.input}
-        placeholder="Username"
-        value={username}
-        onChangeText={setUsername}
-      />
-      <TextInput
-        style={styles.input}
-        placeholder="Email"
-        value={email}
-        onChangeText={setEmail}
-      />
-      <TextInput
-        style={styles.input}
-        placeholder="Phone"
-        value={phone}
-        onChangeText={setPhone}
-      />
-      <TextInput
-        style={styles.input}
-        placeholder="Password"
-        secureTextEntry
-        value={password}
-        onChangeText={setPassword}
-      />
-      <TextInput
-        style={styles.input}
-        placeholder="Confirm Password"
-        secureTextEntry
-        value={confirmPassword}
-        onChangeText={setConfirmPassword}
-      />
+        <View style={styles.title2}>
+          <Text style={styles.title3}>Already have an account?</Text>
+          <TouchableOpacity  onPress={() => router.push('/SignInScreen')}>
+            <Text style={styles.title4}>Sign In!</Text>
+          </TouchableOpacity>
+        </View>
+        
+      </View>
+      <View style={styles.container}>
+        
+        <Text style={styles.subin}>Username</Text>
+        <TextInput
+          style={styles.input}
+          placeholder=""
+          value={username}
+          onChangeText={setUsername}
+        />
 
-      <Button title="Sign Up" onPress={handleSignUp} />
-    </View>
+        <Text style={styles.subin}>Email</Text>
+        <TextInput
+          style={styles.input}
+          placeholder=""
+          keyboardType="email-address"
+          value={email}
+          onChangeText={setEmail}
+        />
+        <Text style={styles.subin}>Phone</Text>
+        <TextInput
+          style={styles.input}
+          placeholder=""
+          keyboardType="phone-pad"
+          value={phone}
+          onChangeText={setPhone}
+        />
+
+        <Text style={styles.subin}>Password</Text>
+        <TextInput
+          style={styles.input}
+          placeholder=""
+          secureTextEntry
+          value={password}
+          onChangeText={setPassword}
+        />
+
+        <Text style={styles.subin}>Confirm password</Text>
+        <TextInput
+          style={styles.input}
+          placeholder=""
+          secureTextEntry
+          value={confirmPassword}
+          onChangeText={setConfirmPassword}
+        />
+
+        <TouchableOpacity style={styles.sign} onPress={handleSignUp}>
+          <Text style={styles.buttonText}>Sign Up</Text>
+        </TouchableOpacity>
+      </View>
+    </ImageBackground>
   );
 }
 
 const styles = StyleSheet.create({
+  background: {
+    flex: 1,
+  },
   container: {
     flex: 1,
-    padding: 20,
+    paddingHorizontal: 15,
     justifyContent: 'center',
+    marginLeft: 15,
   },
   title: {
     fontSize: 24,
     fontWeight: 'bold',
-    marginBottom: 20,
+    alignItems: "baseline",
+    color: '#000000',
+    marginLeft:35,
+    marginTop:20,
+  },
+  title4:{
+    marginLeft:5,
+    color:"#2582ff",
+    fontWeight: 'bold',
+
+  },
+  title3:{
+    color: '#000000',
+
+  },
+  title2:{
+    flexDirection: 'row', // Los elementos se colocarán horizontalmente
+    padding: 5,
+    marginLeft: 27,
+
   },
   input: {
-    height: 40,
-    borderColor: '#ccc',
-    borderWidth: 1,
+    width: 300,
+    padding: 10,
     marginBottom: 15,
-    paddingHorizontal: 10,
+    borderWidth: 1,
+    borderColor: '#ccc',
+    borderRadius: 8,
+    backgroundColor: 'rgba(255, 255, 255, 0.8)', // Fondo semitransparente para mejor visibilidad
+  },
+  subin:{
+    color:'gray',
+    marginBottom:2,
+
+  },
+  sign: {
+    width: 300,
+    backgroundColor: '#2582ff',
+    padding: 10,
+    alignItems: 'center',
+    borderRadius: 5,
+    height: 45,
+    marginTop: 30,
+  },
+  buttonText: {
+    color: '#fff',
+    fontSize: 16,
+    fontWeight: 'bold',
   },
 });
